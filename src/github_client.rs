1fb3f7edb71896aa9f34e9f4207cd798741eebce
Michał Kadłubowski <michkad@gazeta.pl>
2021-12-15 02:22:25 +0100

    Improvements

--- src/github_client.rs
+++ src/github_client.rs
@@ -1,12 +1,23 @@
+use std::{fmt};
+
 use crate::github::{Contributor, PagedResult, Repository};
-use futures::{stream, Stream, StreamExt};
-use reqwest::{header::USER_AGENT, Client, Error};
+use futures::{stream, Stream, StreamExt, TryStreamExt};
+use reqwest::{header::USER_AGENT, Client, RequestBuilder, StatusCode};
 
 const GITHUB_REST_SEARCH_API_URL: &str = "https://api.github.com";
 const CONTRIBUTORS_COUNT_FOR_ANALYSIS: u8 = 25;
 
+#[derive(Debug)]
+pub struct ApiError(pub String);
+
+impl fmt::Display for ApiError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Error - {}", self.0)
+    }
+}
+
 pub struct GithubClient {
-    auth_token: String,
+    auth_token: Option<String>,
     client: Client,
 }
 
@@ -16,95 +27,96 @@ pub struct ContributionResult {
     pub percentage: f32,
 }
 
-fn calc_most_contributing_percentage(mut data: Vec<Contributor>) -> Result<(String, f32), Error> {
-    data.sort_by(|a, b| b.contributions.cmp(&a.contributions));
-    let all_contrubutions: u32 = data.iter().map(|x| x.contributions).sum();
-    let most_contributing_user_percent = data
-        .iter()
-        .map(|x| {
-            (
-                x.login.to_string(),
-                (x.contributions as f32) / (all_contrubutions as f32),
-            )
-        })
-        .next();
-    Ok(most_contributing_user_percent.unwrap())
-}
-
 impl GithubClient {
-    pub fn new(auth_token: String) -> Self {
+    pub fn new(auth_token: Option<String>) -> Self {
         Self {
             auth_token,
             client: Client::new(),
         }
     }
 
-    async fn get_top_contributor_percentage(&self, repo: Repository) -> ContributionResult {
+    fn create_get_request_builder(&self, url: &str) -> RequestBuilder {
+        let builder = 
+        match &self.auth_token {
+            Some(token) => self.client.get(url).bearer_auth(token),
+            None =>  self.client.get(url)   
+        };
+        builder.header(USER_AGENT, "request")
+    }
+
+    fn calc_most_contributing_percentage(mut data: Vec<Contributor>) -> Option<(String, f32)> {
+        if data.len() == 0 {
+            return None
+        }
+    
+        data.sort_by(|a, b| b.contributions.cmp(&a.contributions));
+        let all_contrubutions: u32 = data.iter().map(|x| x.contributions).sum();
+        Some ((data[0].login.to_string(), (data[0].contributions as f32) / (all_contrubutions as f32)))
+    }
+
+    async fn get_top_contributor_percentage(&self, repo: Repository) -> Result<ContributionResult, ApiError> {
         let request_url = format!(
             "{}/{}/{}/{}/contributors",
             GITHUB_REST_SEARCH_API_URL, "repos", repo.owner.login, repo.name
         );
-        let response = self
-            .client
-            .get(&request_url)
+        let response = self.create_get_request_builder(&request_url)
             .query(&[
                 ("sort", "contributions"),
                 ("o", "desc"),
                 ("per_page", &CONTRIBUTORS_COUNT_FOR_ANALYSIS.to_string()),
             ])
-            .header(USER_AGENT, "request")
-            .bearer_auth(self.auth_token.to_string())
             .send()
             .await
-            .unwrap();
+            .map_err(|_x| ApiError("Error calling API".to_owned()))?;
 
         //println!("Calling for 25 cotntributors Res: {}", response.status());
-
-        let data: Vec<Contributor> = response.json().await.unwrap();
-        let most_contribution = calc_most_contributing_percentage(data).unwrap();
-        ContributionResult {
-            repo: repo.name.to_string(),
+        if response.status() != StatusCode::OK{
+            return Err(ApiError(format!("Error calling API - response code: {}", response.status())))
+    }
+        let data: Vec<Contributor> = response.json().await.map_err(|_x| ApiError("Error deserializing contributors data".to_owned()))?;
+        let most_contribution = Self::calc_most_contributing_percentage(data).unwrap_or(("N/A".to_owned(),0 as f32));
+        Ok(ContributionResult {
+            repo: repo.name.to_owned(),
             username: most_contribution.0,
             percentage: most_contribution.1,
-        }
+        })
     }
 
-    async fn get_repos_page<'a>(&self, lang: &str, i: i32) -> Result<PagedResult<Repository>, Error> {
+    async fn get_repos_page<'a>(&self, lang: &str, i: i32) -> Result<PagedResult<Repository>,ApiError> {
         let request_url = format!(
             "{}/{}/{}",
             GITHUB_REST_SEARCH_API_URL, "search", "repositories"
         );
         let lang_param = &format!("language:{}", lang)[..];
-        let response = self
-            .client
-            .get(&request_url)
+        let response = self.create_get_request_builder(&request_url)
             .query(&[
                 ("q", lang_param),
                 ("sort", "stars"),
                 ("o", "desc"),
                 ("page", &i.to_string()),
             ])
-            .header(USER_AGENT, "request")
-            .bearer_auth(self.auth_token.to_string())
             .send()
-            .await?;
+            .await
+            .map_err(|_x| ApiError("Error calling API".to_owned()))?;
 
         //println!("Calling for page: {}", response.status());
+        if response.status() != StatusCode::OK{
+                return Err(ApiError(format!("Error calling API - response code: {}", response.status())))
+        }
 
-        let data: PagedResult<Repository> = response.json().await?;
+        let data: PagedResult<Repository> = response.json().await.map_err(|_x| ApiError("Error deserializing repos data".to_owned()))?;
         Ok(data)
     }
 
-    pub fn get_pages<'l>(&'l self, lang: &'l str) -> impl Stream<Item = Repository> + 'l {
+    pub fn get_pages<'l>(&'l self, lang: &'l str) -> impl Stream<Item = Result<Repository,ApiError>> + 'l {
         stream::iter(0..)
             .then(move |i| self.get_repos_page(&lang, i))
-            .map(|x| x.unwrap().items)
-            .flat_map(|page| stream::iter(page))
+            .map_ok(|x| stream::iter(x.items.into_iter().map(Ok))).try_flatten()
     }
 
-    pub fn get_contributors<'l>(&'l self, lang: &'l str, i: usize) -> impl Stream<Item = ContributionResult> + 'l {
+    pub fn get_contributors<'l>(&'l self, lang: &'l str, project_count: usize) -> impl Stream<Item = Result<ContributionResult,ApiError>> + 'l {
         self.get_pages(&lang)
-            .then(move |repo: Repository| self.get_top_contributor_percentage(repo))
-            .take(i)
+            .and_then(move |repo: Repository| self.get_top_contributor_percentage(repo))
+            .take(project_count)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use crate::github_client::GithubClient;
 
 #[tokio::main]
 async fn main() {
-    let x = App::new("rust-rest-client-example")
+    let arg_matches = App::new("rust-rest-client-example")
                 .version("1.0")
                 .after_help("USE \"gh_token\" ENV VARIABLE TO PASS GITHUB ACCESS TOKEN")
                 .author("Michał Kadłubowski")
@@ -28,15 +28,21 @@ async fn main() {
                     .takes_value(true))
                 .get_matches();
 
-    // TODO handle bad input better                
-    let lang = x.value_of("LANG").unwrap();
-    let count = x.value_of("COUNT").unwrap().parse::<i32>().unwrap();
-    let token =  env::var("gh_token").unwrap();
+    // Validated by clap so unwrap is mostly ok
+    let lang = arg_matches.value_of("LANG").unwrap();
+    let count = arg_matches.value_of("COUNT").unwrap().parse::<i32>().unwrap();
+    let token =  env::var("gh_token").ok();
     
-    let client = GithubClient::new(token.to_string());
+    let client = GithubClient::new(token);
 
     let mut res  = Box::pin(client.get_contributors(&lang, count as usize));
     while let Some(i) = res.next().await {
-        println!("project: {0: <20} user: {1: <20} percentage: {2: <10}", i.repo, i.username, i.percentage);
+        match i {
+            Ok(result ) => println!("project: {0: <20} user: {1: <20} percentage: {2: <10}", result.repo, result.username, result.percentage),
+            Err(api_error ) => {
+                println!("Something failed - {}", api_error.0);
+                break;
+            }
+        }
     }
 }
